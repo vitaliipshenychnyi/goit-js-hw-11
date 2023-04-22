7ca3700d4c93ef794d756a4df62c9e0f8c9db1ba
VitaliiPshenychniy <4kvaap@gmail.com>
2023-04-22 23:49:59 +0300

    async

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import './css/styles.css';
 import refs from './js/refs';
@@ -8,24 +8,71 @@ import { getImages } from './js/get_images';
 let currentPage = 1;
 
 // Ініціалізація бібліотеки SimpleLightbox
-// const gallery = new SimpleLightbox('.gallery a');
+const gallery = new SimpleLightbox('.gallery a');
 
 refs.search.addEventListener('submit', onSearch);
 refs.loadmore.addEventListener('click', onLoad);
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
   refs.card.innerHTML = '';
   refs.loadmore.hidden = true;
   if (!refs.search.elements.searchQuery.value) {
     return;
   }
-  getImages(refs.search.elements.searchQuery.value);
-  // gallery.refresh();
+  try {
+    const cardImage = await getImages(refs.search.elements.searchQuery.value);
+    // gallery.refresh();
+    return cardImage;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function onLoad() {
+async function onLoad() {
   currentPage += 1;
-  getImages(refs.search.elements.searchQuery.value, currentPage);
-  // gallery.refresh();
+  try {
+    const addImages = await getImages(
+      refs.search.elements.searchQuery.value,
+      currentPage
+    );
+    // gallery.refresh();
+    return addImages;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// ===== Код через fetch, then, catch =====
+
+// // import SimpleLightbox from 'simplelightbox';
+// // import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import './css/styles.css';
+// import refs from './js/refs';
+// import { getImages } from './js/get_images';
+
+// let currentPage = 1;
+
+// // Ініціалізація бібліотеки SimpleLightbox
+// // const gallery = new SimpleLightbox('.gallery a');
+
+// refs.search.addEventListener('submit', onSearch);
+// refs.loadmore.addEventListener('click', onLoad);
+
+// function onSearch(event) {
+//   event.preventDefault();
+//   refs.card.innerHTML = '';
+//   refs.loadmore.hidden = true;
+//   if (!refs.search.elements.searchQuery.value) {
+//     return;
+//   }
+//   getImages(refs.search.elements.searchQuery.value);
+//   // gallery.refresh();
+// }
+
+// function onLoad() {
+//   currentPage += 1;
+//   getImages(refs.search.elements.searchQuery.value, currentPage);
+//   // gallery.refresh();
+// }
